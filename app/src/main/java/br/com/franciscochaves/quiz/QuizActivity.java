725970ebb0412b92aec5bd10ddb7d58b6721db06
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-02 12:32:18 -0300

    Adicionando outras questões no quiz

--- app/src/main/java/br/com/franciscochaves/quiz/QuizActivity.java
+++ app/src/main/java/br/com/franciscochaves/quiz/QuizActivity.java
@@ -8,6 +8,9 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizActivity extends AppCompatActivity {
 
     private TextView pergunta;
@@ -19,6 +22,15 @@ public class QuizActivity extends AppCompatActivity {
 
     private int respostaCerta;
     private int pontos = 0;
+
+    List<Questao> questoes = new ArrayList<Questao>(){
+        {
+            add(new Questao("Quem descobriu o Brasil?", R.id.radio_resposta1, "Pedro Álvares Cabral", "Cristóvão Colombo", "Donald Trump", "Francisco Chaves"));
+            add(new Questao("Qual o melhor site ?", R.id.radio_resposta2, "www.g1.com", "franciscochaves.com.br", "www.facebook.com", "www.twitter.com"));
+            add(new Questao("Qual o melhor político do Brasil?", R.id.radio_resposta3, "Tiririca", "Eneas Carneiro", "Nenhum presta", "Todos são bons"));
+            add(new Questao("Qual a melhor plataforma mobile?", R.id.radio_resposta4, "Symbian", "BlackBerry", "iOS", "Android <<<"));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +44,7 @@ public class QuizActivity extends AppCompatActivity {
         rbResposta4 = findViewById(R.id.radio_resposta4);
         radioGroup = findViewById(R.id.radio_grup_pergunta);
 
-        respostaCerta = R.id.radio_resposta1;
+        carregarQuestao();
     }
 
     public void btnResponderOnclick(View view ) {
@@ -55,12 +67,17 @@ public class QuizActivity extends AppCompatActivity {
     protected void onRestart(){
         super.onRestart();
 
-        pergunta.setText("Qual é o melhor site?");
-        rbResposta1.setText("www.g1.com");
-        rbResposta2.setText("franciscochaves.com.br");
-        rbResposta3.setText("www.facebook.com");
-        rbResposta4.setText("www.twitter.com");
+        carregarQuestao();
+    }
 
-        respostaCerta = R.id.radio_resposta2;
+    private void carregarQuestao(){
+        Questao q = questoes.remove(0);
+        pergunta.setText(q.getPergunta());
+        List<String> resposta = q.getRespostas();
+        rbResposta1.setText(resposta.get(0));
+        rbResposta2.setText(resposta.get(1));
+        rbResposta3.setText(resposta.get(2));
+        rbResposta4.setText(resposta.get(3));
+        respostaCerta = q.getRespostaCerta();
     }
 }
