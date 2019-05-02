2c0b6c2994b2abfda6b251ba6d35ec0d338f2fc9
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-02 00:10:44 -0300

    Adicionando tempo de duração da tela resposta

--- app/src/main/java/br/com/franciscochaves/quiz/RespostaActivity.java
+++ app/src/main/java/br/com/franciscochaves/quiz/RespostaActivity.java
@@ -9,5 +9,20 @@ public class RespostaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta);
+        getSupportActionBar().hide();
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finish();
+            }
+        });
+
+        thread.start();
     }
 }
