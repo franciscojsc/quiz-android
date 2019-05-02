e68633075fcb7bafe0771aef7635ceb88555bf52
Francisco Chaves <francisco.jschaves@gmail.com>
2019-05-02 00:24:04 -0300

    Adicionando verificação da resposta

--- app/src/main/java/br/com/franciscochaves/quiz/RespostaActivity.java
+++ app/src/main/java/br/com/franciscochaves/quiz/RespostaActivity.java
@@ -1,7 +1,10 @@
 package br.com.franciscochaves.quiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RespostaActivity extends AppCompatActivity {
 
@@ -10,6 +13,19 @@ public class RespostaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resposta);
         getSupportActionBar().hide();
+
+        ImageView imgResposta = findViewById(R.id.image_resposta);
+        TextView resposta = findViewById(R.id.text_mensagem_resposta);
+
+        Intent intent = getIntent();
+        boolean acertou = intent.getBooleanExtra("acertou", false);
+        if(acertou){
+            imgResposta.setImageResource(R.drawable.acertou);
+            resposta.setText("Acertou!");
+        }else{
+            imgResposta.setImageResource(R.drawable.errou);
+            resposta.setText("Errou!");
+        }
 
         Thread thread = new Thread(new Runnable() {
             @Override
